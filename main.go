bba49f0f1355a7411a42dce09d2a9b159e117e6b
Frenek Lopez <eric.frenek.lr@gmail.com>
2025-10-08 09:36:36 -0700

    Channel y Gorutines

--- main.go
+++ main.go
@@ -1,29 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println(text)
+func message(text string, c chan string) {
+	c <- text
 }
 
 func main() {
-	var wg sync.WaitGroup
+	c := make(chan string, 3)
+	c <- "Email1"
+	c <- "Email2"
+	c <- "Email3"
 
-	fmt.Println("Hello")
-	wg.Add(1)
+	// Close
+	close(c)
 
-	go say("world", &wg)
+	//fmt.Println(len(c), cap(c))
 
-	wg.Wait()
-	go func(text string) {
-		fmt.Println(text)
-	}("Adios")
+	//c <- "Mensaje 3"
+	for saludo := range c {
+		fmt.Println(saludo)
+	}
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+	email3 := make(chan string)
 
-	time.Sleep(time.Second * 1)
+	go message("Mensaje1", email1)
+	go message("Mensaje2", email2)
+	go message("Mensaje3", email3)
+	for i := 0; i < 3; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		case m3 := <-email3:
+			fmt.Println("Email recibido de email3", m3)
+		}
 
+	}
 }
